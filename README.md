89bcf175ddd7eac2a81aad66b39921110c507571
David Bingham <dbinh@allstate.com>
2018-02-21 17:03:08 +0000

    Adding a camel example in boot

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # camel
 Experimenting with Apache Camel
+
+Camel is an implementation of the Mediator pattern (encapsulating logic for how components interact) supporting many kinds of communication protocols.
+Internally it implements all of the Enterprise Integration Patterns.
+
+Key components of Camel and Enterprise Integration Patterns:
+
+Message - metadata and payload
+Exchange - incoming and outgoing
+Endpoint - a way to connect to a messaging channel
+Channel - how applications communicate with each other - often implemented through a message queue
+Pipes and Filters - a way to separate concerns when components interact with each other - core to camel and implementation of routing rules.
+
---
+++ src/test/java/com/spring/demo/routes/SpringCamelApplicationTests.java
@@ -0,0 +1,52 @@
+package com.spring.demo.routes;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest
+public class SpringCamelApplicationTests {
+
+    @EndpointInject(uri = MOCK_RESULT)
+    private MockEndpoint resultEndpoint;
+
+    @Autowired
+    private CamelContext camelContext;
+
+    @EndpointInject(uri = MOCK_TIMER)
+    private ProducerTemplate producer;
+
+    private static final String MOCK_RESULT = "mock:result";
+    private static final String MOCK_TIMER = "direct:mock-timer";
+
+    @Before
+    public void setup() throws Exception {
+        camelContext.getRouteDefinition(TimerRoute.ROUTE_NAME)
+                .autoStartup(true)
+                .adviceWith(camelContext, new AdviceWithRouteBuilder() {
+                    @Override
+                    public void configure() throws Exception {
+                        replaceFromWith(MOCK_TIMER);
+                        interceptSendToEndpoint("log*")
+                                .skipSendToOriginalEndpoint()
+                                .to(MOCK_RESULT);
+                    }
+                });
+    }
+
+    @Test
+    public void sendMessage() throws Exception {
+        resultEndpoint.expectedMessageCount(1);
+        producer.sendBody("A message");
+        resultEndpoint.assertIsSatisfied();
+    }
+}
